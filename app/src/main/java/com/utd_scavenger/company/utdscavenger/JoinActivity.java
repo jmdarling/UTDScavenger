1cb71a1303225d412103b6ad61232d583918e726
Stephen <Sack11man@aol.com>
2015-03-25 19:20:33 -0500

    The join activty now accepts NFC tags.

--- app/src/main/java/com/utd_scavenger/company/utdscavenger/JoinActivity.java
+++ app/src/main/java/com/utd_scavenger/company/utdscavenger/JoinActivity.java
@@ -1,7 +1,21 @@
 package com.utd_scavenger.company.utdscavenger;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.NfcF;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
 
 /**
  * Handles the process of joining an existing game.
@@ -10,12 +24,19 @@ import android.os.Bundle;
  */
 public class JoinActivity extends Activity {
 
+    private TextView mTextView;
+    private NfcAdapter mNfcAdapter;
+    private PendingIntent mPendingIntent;
+    private IntentFilter[] mIntentFilters;
+    private String[][] mNFCTechLists;
+
     /**
      * Called when the activity is starting. This is where most initialization
      * should go.
      *
      * @param savedInstanceState If the activity is being re-initialized after
      *                           previously being shut down then this Bundle
+     *
      *                           contains the data it most recently supplied in
      *                           onSaveInstanceState(Bundle). Otherwise it is
      *                           null.
@@ -26,5 +47,88 @@ public class JoinActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        /*Set the textview for information output by the NFC Tag*/
+        mTextView = (TextView)findViewById(R.id.tv);
+
+        /*instantiate the the NFC Adapter*/
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        /*Check to see if the device supports NFC*/
+        if (mNfcAdapter != null) {
+            /*Inital text in the textview*/
+            mTextView.setText("Read an NFC tag");
+        } else {
+            /*if the device does not support NFC*/
+            mTextView.setText("This phone is not NFC enabled.");
+        }
+
+        // create an intent with the NFC tag data and deliver to the Join Activity
+        mPendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        // set an intent filter for all MIME data
+        IntentFilter ndefIntent = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        try {
+            ndefIntent.addDataType("*/*");
+            mIntentFilters = new IntentFilter[] { ndefIntent };
+        } catch (Exception e) {
+            Log.e("TagDispatch", e.toString());
+        }
+
+        mNFCTechLists = new String[][] { new String[] { NfcF.class.getName() } };
+
+    }
+
+
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        String action = intent.getAction();
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+        String s = action + "\n\n" + tag.toString();
+
+        // parse through all NDEF messages and their records and pick text type only
+        Parcelable[] data = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        if (data != null) {
+            try {
+                for (int i = 0; i < data.length; i++) {
+                    NdefRecord[] recs = ((NdefMessage)data[i]).getRecords();
+                    for (int j = 0; j < recs.length; j++) {
+                        if (recs[j].getTnf() == NdefRecord.TNF_WELL_KNOWN &&
+                                Arrays.equals(recs[j].getType(), NdefRecord.RTD_TEXT)) {
+                            byte[] payload = recs[j].getPayload();
+                            String textEncoding = ((payload[0] & 0200) == 0) ? "UTF-8" : "UTF-16";
+                            int langCodeLen = payload[0] & 0077;
+
+                            s += ("\n\nNdefMessage[" + i + "], NdefRecord[" + j + "]:\n\"" +
+                                    new String(payload, langCodeLen + 1, payload.length - langCodeLen - 1,
+                                            textEncoding) + "\"");
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                Log.e("TagDispatch", e.toString());
+            }
+        }
+
+        mTextView.setText(s);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (mNfcAdapter != null)
+            mNfcAdapter.enableForegroundDispatch(this, mPendingIntent, mIntentFilters, mNFCTechLists);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (mNfcAdapter != null)
+            mNfcAdapter.disableForegroundDispatch(this);
     }
 }
