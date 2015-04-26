49fbf376ce2b6afc062ee30fd74da51db7c3e683
Jonathan Darling <jmdarling@me.com>
2015-04-26 18:04:14 -0500

    Cleans up CreateStepTwoActivity.

--- app/src/main/java/com/utd_scavenger/company/utdscavenger/CreateStepTwoActivity.java
+++ app/src/main/java/com/utd_scavenger/company/utdscavenger/CreateStepTwoActivity.java
@@ -19,21 +19,36 @@ import com.utd_scavenger.company.utdscavenger.Helpers.NfcHelper;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Handles the process of creating a new game. Specifically, adding the players.
+ *
+ * Written by Jonathan Darling and Stephen Kuehl
+ */
 public class CreateStepTwoActivity extends Activity {
-
     // Helpers.
     NfcHelper mNfcHelper;
 
     // Storage.
     ArrayList<Item> mItems;
 
+    /**
+     * Called when the activity is starting. This is where most initialization
+     * should go.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle
+     *                           contains the data it most recently supplied in
+     *                           onSaveInstanceState(Bundle). Otherwise it is
+     *                           null.
+     *
+     * Written by Jonathan Darling and Stephen Kuehl
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_steptwo);
 
-        // Set up helpers.
+        // Set up NfcHelper.
         try {
             mNfcHelper = new NfcHelper(this, getClass());
         } catch (NfcNotAvailableException | NfcNotEnabledException e) {
@@ -52,30 +67,9 @@ public class CreateStepTwoActivity extends Activity {
         mNfcHelper.setNdefPushMessage(ndefMessage);
     }
 
-    public void onClickDone (View view){
-        new AlertDialog.Builder(this)
-                .setTitle("Start Game")
-                .setMessage("Are you sure you have added all your players?")
-                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        // continue with delete
-                        Intent myIntent = new Intent(getBaseContext(), MainActivity.class);
-                        startActivity(myIntent);
-                    }
-                })
-                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        // do nothing
-                    }
-                })
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .show();
-
-    }
-
     /**
-     * Called after onRestoreInstanceState, onRestart, or onPause, for your
-     * activity to start interacting with the user.
+     * Called after onRestoreInstanceState, onRestart, or onPause.
+     * Enables foreground dispatch.
      *
      * Written by Jonathan Darling and Stephen Kuehl
      */
@@ -86,5 +80,31 @@ public class CreateStepTwoActivity extends Activity {
         // Enable foreground dispatch. This will ensure that when the NFC tag is
         // read, it will immediately be processed by this activity.
         mNfcHelper.enableForegroundDispatch();
+    }
+
+    /**
+     * Click listener for the Done button.
+     *
+     * @param view The view that was clicked.
+     *
+     * Written by Stephen Kuehl
+     */
+    public void onClickDone (View view){
+        new AlertDialog.Builder(this)
+                .setTitle("Start Game")
+                .setMessage("Are you sure you have added all your players?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent myIntent = new Intent(getBaseContext(), MainActivity.class);
+                        startActivity(myIntent);
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Do nothing.
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
